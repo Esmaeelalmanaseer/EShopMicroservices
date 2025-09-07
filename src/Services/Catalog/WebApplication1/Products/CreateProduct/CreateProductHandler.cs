5744cbaebe7ad0ab5d500a3e0218761152232aad
Esmaeel <ismael.manaseer@gmail.com>
2025-09-07 16:57:27 +0300

    setup

---
+++ src/Services/Catalog/WebApplication1/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,28 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    :ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        //Mapping
+        var prodcut = new Product
+        {
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price, 
+            Name = command.Name,
+        };
+        // Save Data Base
+
+        //return Result
+        return new CreateProductResult(Guid.NewGuid());
+    }
+}
