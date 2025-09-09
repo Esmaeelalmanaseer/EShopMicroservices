a9419ccb7c6aa083cbb1796394a3c1173b512c16
Esmaeel <ismael.manaseer@gmail.com>
2025-09-09 17:02:53 +0300

    dn

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.API.Exceptions;
+using Marten.Linq.QueryHandlers;
+
+namespace Catalog.API.Products.GetProductById;
+public record GetProductByIdQuery(Guid id) : IQuery<GetProductByIdResult>;
+public record GetProductByIdResult(Product Product);
+public class GetProductByIdQueryHandler(IDocumentSession session, ILogger logger) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductByIdResult.Handle called with {@Query}", query);
+        var product = await session.LoadAsync<Product>(query.id, cancellationToken);
+        if (product is null)
+            throw new ProductNotFoundException();
+        return new GetProductByIdResult(product);
+
+    }
+}
