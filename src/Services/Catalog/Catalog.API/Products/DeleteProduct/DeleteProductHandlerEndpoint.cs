95806f28c4797a46301f03fbac2399113c96c8a1
Esmaeel <ismael.manaseer@gmail.com>
2025-10-28 12:08:38 +0300

    Delete Endpoint

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandlerEndpoint.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductResponse(bool IsSuccess);
+public class DeleteProductHandlerEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/product/{id}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteProductCommand(id));
+            var response = result.Adapt<DeleteProductResponse>();
+            return Results.Ok(response);
+        }).WithName("DeleteProduct")
+        .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Delete PRoduct")
+        .WithDescription("Delte Product");
+    }
+}
